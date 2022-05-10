a0382481983602d50d26c6bfa9b3fa11d6dc51d7
dirk.he <hejiandirk@163.com>
2022-05-10 16:54:25 +0800

    update

--- src/api/map.ts
+++ src/api/map.ts
@@ -2,7 +2,7 @@ import settings from '@/settings.json';
 export const loadProvinceJson = (data: any): any => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://dirkhe1051931999.github.io/map/province/${data.prov}.json`, true);
+    xhr.open('GET', `https://dirkhe1051931999.github.io/quasar/map/province/${data.prov}.json`, true);
     xhr.send();
     xhr.onreadystatechange = function(e) {
       if (xhr.readyState == 4 && xhr.status == 200) {
@@ -14,7 +14,7 @@ export const loadProvinceJson = (data: any): any => {
 export const loadCityJson = (data: any): any => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://dirkhe1051931999.github.io/map/city/${data.city}.json`, true);
+    xhr.open('GET', `https://dirkhe1051931999.github.io/quasar/map/city/${data.city}.json`, true);
     xhr.send();
     xhr.onreadystatechange = function(e) {
       if (xhr.readyState == 4 && xhr.status == 200) {
