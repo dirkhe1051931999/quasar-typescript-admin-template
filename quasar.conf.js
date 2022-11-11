4f348ec339e546edb2aab88240a201747a83c5cd
dirk.he <hejiandirk@163.com>
2022-11-11 11:02:08 +0800

    fix: build bug

--- quasar.conf.js
+++ quasar.conf.js
@@ -51,7 +51,7 @@ module.exports = configure(function (ctx) {
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       transpile: false,
-      publicPath: ctx.dev === 'prod' ? setting.publicPath : '/',
+      publicPath: ctx.prod ? setting.publicPath : '/',
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
       // Applies only if "transpile" is set to true.
